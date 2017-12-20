81210e48b0eb19538fd88cbbe354291b4a68379e
lleventhal <34609956+lleventhal@users.noreply.github.com>
2017-12-20 13:29:52 -0500

    Add files via upload

--- all_brass_Dec16.R
+++ all_brass_Dec16.R
@@ -3,7 +3,10 @@ library(MasterBayes)
 library(dplyr)
 
 #Parameters
-Vrs<-c(1:14) #variables want to calculate
+group<-"All" #or "Batch"
+Plot<-"Track" #pick the batch you want
+Type<-"Part"
+Vrs<-c(15:40) #variables want to calculate
 trait.fun<-function(Plot, type="Full"){
   if (Plot == "B30" | Plot == "Comp") {
     if (type == "Full") {traits<-c(1:14,41:43)} else if (
@@ -24,10 +27,12 @@ included_loci<-c(1:10)
 number_loci<-length(included_loci)
 epsilon1<-c(0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005)	#allelic dropout
 epsilon2<-c(0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005)	#stochastic (misscore) error
-group<-"All" #or "All"
-Plot<-"Comp" #pick the batch you want
-Type<-"Part"
-p.list<-c(2:15) #column indices for calculating z-scores
+if (Plot == "B30" | Plot == "Comp"){
+  p.list<-c(2:15) #column indices for calculating z-scores
+} else if (Plot == "Track" | Plot == "Fwest"){
+  p.list<-c(2:15) #column indices for calculating z-scores #need to figure out
+}
+
 
 #Functions
 zscore<-function(X){(X-mean(X))/sd(X)} 	#function to standardize traits from Austen
@@ -84,7 +89,7 @@ if (group == "All") {
     BrapaG<-BrapaG[-grep("2808c_FWest_bG_F|2809c_FWest_bG_F|2810c_FWest_bG_F|2811c_FWest_bG_F|2814c_FWest_bG_F",BrapaG$id),]
     
     BrapaG<-rbind(BrapaG,fwest_probG)
-  } #end if statement over Fwest problem
+  } #end if statement over Fwest problem#dont need because did in earlier code?
   if (Plot == "B30" | Plot == "Comp"){
     BrapaG<-BrapaG[-grep("bA2",BrapaG$timevar),] 
     BrapaP<-BrapaP[-grep("bA2",BrapaP$timevar),]
@@ -103,7 +108,7 @@ if (group == "All") {
     flwcount$id<-paste("Track",flwcount$pos, sep="_")
   }#end of if statement adding pheno data to Track
   if (Plot == "Fwest") {
-    flwcount<-read.csv(paste("fwphenZerosJune7-2013.csv.csv", sep=""))
+    flwcount<-read.csv(paste("fwphenZerosJune7-2013.csv", sep=""))
     flwcount$id<-paste("Fwest",flwcount$pos, sep="_")
   }#end of if statement adding pheno data to Fwest
   
@@ -127,33 +132,63 @@ if (group == "All") {
     BrapaP_test[k,]<-SimplifiedIntervals }#end loop for creation of dur and sum column
   BrapaP_male<-cbind(BrapaP_test,flwr_test)
   
-  
-  BrapaP_male$batchdurA<-rowMeans(BrapaP_male[,3:4])
-  BrapaP_male$batchdurB<-rowMeans(BrapaP_male[,5:6])
-  BrapaP_male$batchdurB2<-rowMeans(BrapaP_male[,6:8])
-  BrapaP_male$batchdurC<-rowMeans(BrapaP_male[,8:9])
-  BrapaP_male$batchdurD<-rowMeans(BrapaP_male[,9:10])
-  BrapaP_male$batchdurD2<-rowMeans(BrapaP_male[,10:11])
-  BrapaP_male$batchdurE<-rowMeans(BrapaP_male[,11:19])
-  
-  BrapaP_male<-BrapaP_male[,-c(1:19)]
-  BrapaP_male<-merge(BrapaP_male,flwcount,by="id")
-  
-  BrapaP_male$batchsumA<-rowSums(BrapaP_male[20:21]) #bA
-  BrapaP_male$batchsumB<-rowSums(BrapaP_male[22:23])#bB
-  BrapaP_male$batchsumB2<-rowSums(BrapaP_male[23:25])#bB2
-  BrapaP_male$batchsumC<-rowSums(BrapaP_male[25:26])#bC
-  BrapaP_male$batchsumD<-rowSums(BrapaP_male[26:27])#bD
-  BrapaP_male$batchsumD2<-rowSums(BrapaP_male[27:28])#bD2
-  BrapaP_male$batchsumE<-rowSums(BrapaP_male[28:36])#bE
-  
-  BrapaP_male<-BrapaP_male[,-c(2:10,18:40)]
+  if (Plot == "B30" | Plot == "Comp"){
+    BrapaP_male$batchdurA<-rowMeans(BrapaP_male[,3:4])
+    BrapaP_male$batchdurB<-rowMeans(BrapaP_male[,5:6])
+    BrapaP_male$batchdurB2<-rowMeans(BrapaP_male[,6:8])
+    BrapaP_male$batchdurC<-rowMeans(BrapaP_male[,8:9])
+    BrapaP_male$batchdurD<-rowMeans(BrapaP_male[,9:10])
+    BrapaP_male$batchdurD2<-rowMeans(BrapaP_male[,10:11])
+    BrapaP_male$batchdurE<-rowMeans(BrapaP_male[,11:19])
+    
+    BrapaP_male<-BrapaP_male[,-c(1:19)]
+    BrapaP_male<-merge(BrapaP_male,flwcount,by="id")
+    
+    BrapaP_male$batchsumA<-rowSums(BrapaP_male[20:21]) #bA
+    BrapaP_male$batchsumB<-rowSums(BrapaP_male[22:23])#bB
+    BrapaP_male$batchsumB2<-rowSums(BrapaP_male[23:25])#bB2
+    BrapaP_male$batchsumC<-rowSums(BrapaP_male[25:26])#bC
+    BrapaP_male$batchsumD<-rowSums(BrapaP_male[26:27])#bD
+    BrapaP_male$batchsumD2<-rowSums(BrapaP_male[27:28])#bD2
+    BrapaP_male$batchsumE<-rowSums(BrapaP_male[28:36])#bE
+    
+    BrapaP_male<-BrapaP_male[,-c(2:10,18:40)]
+    
+  } #end of loop calculating row means and row sums
   
   if (Plot == "Fwest" | Plot == "Track"){
-    BrapaP<- BrapaP %>% mutate(., batchdurG = mean(12:14)) %>% mutate(., batchdurG = mean(14:16))%>% mutate(., batchdurI = mean(16:18))%>% mutate(., batchdurJ = mean(18:20))%>% mutate(., batchdurK = mean(20:22))%>% mutate(., batchdurL = mean(22:24))%>% mutate(., batchdurM = mean(24:26))%>% mutate(., batchdurN = mean(26:28))%>% mutate(., batchdurN2 = mean(28:30))%>% mutate(., batchdurO = mean(30:31))%>% mutate(., batchdurO2 = mean(31:32))%>% mutate(., batchdurP = mean(32:33))%>% mutate(., batchdurQ = mean(33:34))
-    BrapaP<- flwcount %>% mutate(., batchsumG = mean(12:14)) %>% mutate(., batchsumG = mean(14:16))%>% mutate(., batchsumI = mean(16:18))%>% mutate(., batchsumJ = mean(18:20))%>% mutate(., batchsumK = mean(20:22))%>% mutate(., batchsumL = mean(22:24))%>% mutate(., batchsumM = mean(24:26))%>% mutate(., batchsumN = mean(26:28))%>% mutate(., batchsumN2 = mean(28:30))%>% mutate(., batchsumO = mean(30:31))%>% mutate(., batchsumO2 = mean(31:32))%>% mutate(., batchsumP = mean(32:33))%>% mutate(., batchsumQ = mean(33:34))
+    BrapaP_male$batchdurG<-rowMeans(BrapaP_male[,1:3])
+    BrapaP_male$batchdurH<-rowMeans(BrapaP_male[,3:5])
+    BrapaP_male$batchdurI<-rowMeans(BrapaP_male[,5:7])
+    BrapaP_male$batchdurJ<-rowMeans(BrapaP_male[,7:9])
+    BrapaP_male$batchdurK<-rowMeans(BrapaP_male[,9:11])
+    BrapaP_male$batchdurL<-rowMeans(BrapaP_male[,11:13])
+    BrapaP_male$batchdurM<-rowMeans(BrapaP_male[,13:15])
+    BrapaP_male$batchdurN<-rowMeans(BrapaP_male[,15:17])
+    BrapaP_male$batchdurN2<-rowMeans(BrapaP_male[,17:19])
+    BrapaP_male$batchdurO<-rowMeans(BrapaP_male[,19:20])
+    BrapaP_male$batchdurO2<-rowMeans(BrapaP_male[,20:21])
+    BrapaP_male$batchdurP<-rowMeans(BrapaP_male[,21:22])
+    BrapaP_male$batchdurQ<-rowMeans(BrapaP_male[,22:23])
     
-    BrapaP_male<-BrapaP_male[,-c(2:10,18:40)]#need to alter this to get rid of everything exept the new varibales
+    BrapaP_male<-BrapaP_male[,-c(1:24)]
+    BrapaP_male<-merge(BrapaP_male,flwcount,by="id")
+    
+    BrapaP_male$batchsumG<-rowSums(BrapaP_male[,25:27])
+    BrapaP_male$batchsumH<-rowSums(BrapaP_male[,27:29])
+    BrapaP_male$batchsumI<-rowSums(BrapaP_male[,29:31])
+    BrapaP_male$batchsumJ<-rowSums(BrapaP_male[,31:33])
+    BrapaP_male$batchsumK<-rowSums(BrapaP_male[,33:35])
+    BrapaP_male$batchsumL<-rowSums(BrapaP_male[,35:37])
+    BrapaP_male$batchsumM<-rowSums(BrapaP_male[,37:39])
+    BrapaP_male$batchsumN<-rowSums(BrapaP_male[,39:41])
+    BrapaP_male$batchsumN2<-rowSums(BrapaP_male[,41:43])
+    BrapaP_male$batchsumO<-rowSums(BrapaP_male[,43:44])
+    BrapaP_male$batchsumO2<-rowSums(BrapaP_male[,44:45])
+    BrapaP_male$batchsumP<-rowSums(BrapaP_male[,45:46])
+    BrapaP_male$batchsumQ<-rowSums(BrapaP_male[,46:47])
+    
+    BrapaP_male<-BrapaP_male[,-c(2:10,24:50)] ## need to see which ones to delete
     BrapaP<-merge(BrapaP_male,BrapaP,by="id",all.y=T)
   }#creates batchsum and batch dur proportion variables in Fwest or Track ####fix for fwest and track later
   
@@ -226,9 +261,88 @@ if (group == "All") {
     
   }#end of loop for zscore for B30 and Comp
   
-  #same thing for fwest and track
+  if (Plot == "Fwest" | Plot == "Track"){
+    traits<-trait.fun(Plot, Type)
+    for (t in traits){
+      if (t == 1){batchdurA<-"Duration for batch A"}
+      if (t == 2){batchdurB<-"Duration for batch B"}
+      if (t == 3){batchdurB2<-"Duration for batch B2"}
+      if (t == 4){batchdurC<-"Duration for batch C"}
+      if (t == 5){batchdurD<-"Duration for batch D"}
+      if (t == 6){batchdurD2<-"Duration for batch D2"}
+      if (t == 7){batchdurE<-"Duration for batch E"}
+      if (t == 8){batchsumA<-"Total flowers for batch A"}
+      if (t == 9){batchsumB<-"Total flowers for batch B"}
+      if (t == 10){batchsumB2<-"Total flowers for batch B2"}
+      if (t == 11){batchsumC<-"Total flowers for batch C"}
+      if (t == 12){batchsumD<-"Total flowers for batch D"}
+      if (t == 13){batchsumD2<-"Total flowers for batch D2"}
+      if (t == 14){batchsumE<-"Total flowers for batch E"}
+      
+      if (t == 15){batchdurG<-"Duration for batch G"}
+      if (t == 16){batchdurH<-"Duration for batch H"}
+      if (t == 17){batchdurI<-"Duration for batch I"}
+      if (t == 18){batchdurJ<-"Duration for batch J"}
+      if (t == 19){batchdurK<-"Duration for batch K"}
+      if (t == 20){batchdurL<-"Duration for batch L"}
+      if (t == 21){batchdurM<-"Duration for batch M"}
+      if (t == 22){batchdurN<-"Duration for batch N"}
+      if (t == 23){batchdurN2<-"Duration for batch N2"}
+      if (t == 24){batchdurO<-"Duration for batch O"}
+      if (t == 25){batchdurO2<-"Duration for batch O2"}
+      if (t == 26){batchdurP<-"Duration for batch P"}
+      if (t == 27){batchdurQ<-"Duration for batch Q"}
+      if (t == 28){batchsumG<-"Total flowers for batch G"}
+      if (t == 29){batchsumH<-"Total flowers for batch H"}
+      if (t == 30){batchsumI<-"Total flowers for batch I"}
+      if (t == 31){batchsumJ<-"Total flowers for batch J"}
+      if (t == 32){batchsumK<-"Total flowers for batch K"}
+      if (t == 33){batchsumL<-"Total flowers for batch L"}
+      if (t == 34){batchsumM<-"Total flowers for batch M"}
+      if (t == 35){batchsumN<-"Total flowers for batch N"}
+      if (t == 36){batchsumN2<-"Total flowers for batch N2"}
+      if (t == 37){batchsumO<-"Total flowers for batch O"}
+      if (t == 38){batchsumO2<-"Total flowers for batch O2"}
+      if (t == 39){batchsumP<-"Total flowers for batch P"}
+      if (t == 40){batchsumQ<-"Total flowers for batch Q"}
+      
+      if (t == 41){tot_flwrs<-"Total flowers for a season"}
+      if (t == 42){dur<-"Duration for a season"}
+      if (t == 43){var.dist<-"X and Ycoordinates of an individual"}
+      
+    } #end for loop
+    #grep out all the males so you can z score all of their traits
+    #BrapaP_male<-BrapaP[grep("Male",BrapaP$sex),]
+    foo<-data.frame(do.call('rbind', strsplit(as.character(BrapaP_male$id),'_',fixed=TRUE)))
+    BrapaP_male<-bind_cols(BrapaP_male, foo[2])
+    #names(BrapaP_male)[dim(BrapaP_male)[2]]<-"plantnum"
+    
+    #Convert the traits we want to estimate selection on to z-scores 
+    trait_file<-BrapaP_male
+    p.list<-c(2:27)
+    for (p in p.list){
+      BrapaP_male<-zscore.fun(BrapaP_male,BrapaP_male[,p],paste(names(BrapaP_male)[p],sep="."))
+    }
+    
+  }#end of loop for zscore for Fwest and Track
   #then
-  BrapaP<-merge(BrapaP,BrapaP_male,by="id",all.x =T)
+  if (Plot == "B30" | Plot == "Comp"){
+    #BrapaP<-BrapaP[,-c(2:27)]
+    BrapaP_male<-BrapaP_male[,-c(2:16)]
+    BrapaP<-merge(BrapaP, BrapaP_male, by = "id",all.x=T)
+    #for some reason, this full_join is going way over board... like it should be about 11,000 cells individuals from BrapaP and 1,500 from BrapaP_males to get 12,500. But instead i get 82,902! I am going to see if erasing duplciates fixes this
+    BrapaP<-distinct(BrapaP)
+    # it worked.. woohoo! but still werid...
+  }
+  if (Plot == "Fwest" | Plot == "Track"){
+    BrapaP<-BrapaP[,-c(2:27)]
+    BrapaP_male<-BrapaP_male[,-c(2:28)]
+    BrapaP<-merge(BrapaP, BrapaP_male, by = "id",all.x=T)
+    #for some reason, this full_join is going way over board... like it should be about 11,000 cells individuals from BrapaP and 1,500 from BrapaP_males to get 12,500. But instead i get 82,902! I am going to see if erasing duplciates fixes this
+    BrapaP<-distinct(BrapaP)
+    # it worked.. woohoo! but still werid...
+  }
+
   
   BrapaP_for_MB<-BrapaP
   BrapaG_for_MB<-BrapaG
@@ -264,25 +378,25 @@ if (group == "All") {
     if (v == 19){var19<-expression(varPed(x="z.batchsumK", gender="Male"))}
     if (v == 20){var20<-expression(varPed(x="z.batchsumL", gender="Male"))}
     if (v == 21){var21<-expression(varPed(x="z.batchsumM", gender="Male"))}
-    if (v == 22){var22<-expression(varPed(x="z.batchdurN", gender="Male"))}
-    if (v == 23){var23<-expression(varPed(x="z.batchdurN2", gender="Male"))}
-    if (v == 24){var24<-expression(varPed(x="z.batchsdurO", gender="Male"))}
-    if (v == 25){var25<-expression(varPed(x="z.batchdurO2", gender="Male"))}
-    if (v == 26){var26<-expression(varPed(x="z.batchdurP", gender="Male"))}
-    if (v == 27){var27<-expression(varPed(x="z.batchdurQ", gender="Male"))}
-    if (v == 28){var28<-expression(varPed(x="z.batchsumG", gender="Male"))}
-    if (v == 29){var29<-expression(varPed(x="z.batchsumH", gender="Male"))}
-    if (v == 30){var30<-expression(varPed(x="z.batchsumI", gender="Male"))}
-    if (v == 31){var31<-expression(varPed(x="z.batchsumJ", gender="Male"))}
-    if (v == 32){var32<-expression(varPed(x="z.batchsumK", gender="Male"))}
-    if (v == 33){var33<-expression(varPed(x="z.batchsumL", gender="Male"))}
-    if (v == 34){var34<-expression(varPed(x="z.batchsumM", gender="Male"))}
-    if (v == 35){var35<-expression(varPed(x="z.batchssumN", gender="Male"))}
-    if (v == 36){var36<-expression(varPed(x="z.batchssumN2", gender="Male"))}
-    if (v == 37){var37<-expression(varPed(x="z.batchssumO", gender="Male"))}
-    if (v == 38){var38<-expression(varPed(x="z.batchssumO2", gender="Male"))}
-    if (v == 39){var39<-expression(varPed(x="z.batchssumP", gender="Male"))}
-    if (v == 40){var40<-expression(varPed(x="z.batchssumQ", gender="Male"))}
+    if (v == 22){var22<-expression(varPed(x="z.batchsumN", gender="Male"))}
+    if (v == 23){var23<-expression(varPed(x="z.batchsumN2", gender="Male"))}
+    if (v == 24){var24<-expression(varPed(x="z.batchsumO", gender="Male"))}
+    if (v == 25){var25<-expression(varPed(x="z.batchsumO2", gender="Male"))}
+    if (v == 26){var26<-expression(varPed(x="z.batchsumP", gender="Male"))}
+    if (v == 27){var27<-expression(varPed(x="z.batchsumQ", gender="Male"))}
+    if (v == 28){var28<-expression(varPed(x="z.batchdurG", gender="Male"))}
+    if (v == 29){var29<-expression(varPed(x="z.batchdurH", gender="Male"))}
+    if (v == 30){var30<-expression(varPed(x="z.batchdurI", gender="Male"))}
+    if (v == 31){var31<-expression(varPed(x="z.batchdurJ", gender="Male"))}
+    if (v == 32){var32<-expression(varPed(x="z.batchdurK", gender="Male"))}
+    if (v == 33){var33<-expression(varPed(x="z.batchdurL", gender="Male"))}
+    if (v == 34){var34<-expression(varPed(x="z.batchdurM", gender="Male"))}
+    if (v == 35){var35<-expression(varPed(x="z.batchdurN", gender="Male"))}
+    if (v == 36){var36<-expression(varPed(x="z.batchdurN2", gender="Male"))}
+    if (v == 37){var37<-expression(varPed(x="z.batchdurO", gender="Male"))}
+    if (v == 38){var38<-expression(varPed(x="z.batchdurO2", gender="Male"))}
+    if (v == 39){var39<-expression(varPed(x="z.batchdurP", gender="Male"))}
+    if (v == 40){var40<-expression(varPed(x="z.batchdurQ", gender="Male"))}
     
     if (v == 41){var41<-expression(varPed(x="z.tot_flwrs", gender="Male"))} 
     if (v == 42){var42<-expression(varPed(x="z.dur_flwrs", gender="Male"))} 
@@ -299,14 +413,25 @@ if (group == "All") {
   
   ####GdP####
   GdP<-GdataPed(G=BrapaG_for_MB, categories=NULL, perlocus=T)	#need to specify 'per locus' to allow variable error rates by locus.
-  ####PdP####
-  PdP<-if(sum(Vrs) == 105){PdataPed(formula=list(res.osnotpar, res.osterr, res.phen, var1,var2, var3,var4,var5,var6,var7,var8,var9,var10,var11,var12,var13,var14), data=BrapaP_for_MB)} else if (
-    sum(Vrs) == 715) {PdataPed(formula=list(res.osnotpar, res.osterr, res.phen, var15,var16, var17,var18,var19,var20,var21,var22,var23,var24,var25,var26,var27,var28,var29,var30,var31,var32,var33,var34,var35,var36,var37,var38,var39,var40), data=BrapaP_for_MB)} else if (
-     sum(Vrs) == 126){PdataPed(formula=list(res.osnotpar, res.osterr, res.phen, var41,var42,var.dist), data=BrapaP_for_MB)} else if (
-        sum(Vrs) == 231) {PdataPed(formula=list(res.osnotpar, res.osterr, res.phen, var1,var2, var3,var4,var5,var6,var7,var8,var9,var10,var11,var12,var13,var14,var41,var42,var.dist), data=BrapaP_for_MB)} else if (
-          sum(Vrs) == 841) {PdataPed(formula=list(res.osnotpar, res.osterr, res.phen, var15,var16, var17,var18,var19,var20,var21,var22,var23,var24,var25,var26,var27,var28,var29,var30,var31,var32,var33,var34,var35,var36,var37,var38,var39,var40,var41,var42,var.dist), data=BrapaP_for_MB)}
   
-#PdP<-PdataPed(formula=list(res.osnotpar, res.osterr, res.phen, var1,var2, var3,var4,var5,var6,var7), data=BrapaP_for_MB)
+  mod_subfolder<-"data_files_3/files_generated_by_PatAnalysis_Script"
+  #MODEL: Distance, start date, total flowers, duration of flowering, skew, etc. by batch
+  mod_subfolder<-"data_files/Analysis_Dec_19_LL"	#This empty folder should already be present.
+  
+  
+  
+  ####PdP####
+  #PdP<-if(sum(Vrs) == 105){PdataPed(formula=list(res.osnotpar, res.osterr, res.phen, var1,var2, var3,var4,var5,var6,var7,var8,var9,var10,var11,var12,var13,var14), data=BrapaP_for_MB)} else if (
+ #  sum(Vrs) == 715) {PdataPed(formula=list(res.osnotpar, res.osterr, res.phen, var15,var16, var17,var18,var19,var20,var21,var22,var23,var24,var25,var26,var27,var28,var29,var30,var31,var32,var33,var34,var35,var36,var37,var38,var39,var40), data=BrapaP_for_MB)} else if (
+ #    sum(Vrs) == 126){PdataPed(formula=list(res.osnotpar, res.osterr, res.phen, var41,var42,var.dist), data=BrapaP_for_MB)} else if (
+   #     sum(Vrs) == 231) {PdataPed(formula=list(res.osnotpar, res.osterr, res.phen, var1,var2, var3,var4,var5,var6,var7,var8,var9,var10,var11,var12,var13,var14,var41,var42,var.dist), data=BrapaP_for_MB)} else if (
+  #        sum(Vrs) == 841) {PdataPed(formula=list(res.osnotpar, res.osterr, res.phen, var15,var16, var17,var18,var19,var20,var21,var22,var23,var24,var25,var26,var27,var28,var29,var30,var31,var32,var33,var34,var35,var36,var37,var38,var39,var40,var41,var42,var.dist), data=BrapaP_for_MB)}
+  
+PdP<-PdataPed(formula=list(res.osnotpar, res.osterr, res.phen,var15, var16, var17, var18, var19, var20, var21, var22, var23, var24, var25, var26, var27, var28, var30, var31, var32, var33, var34, var35, var36, var37, var38, var39, var40 ), data=BrapaP_for_MB)
+  #  system is computationally singular: reciprocal condition number = 2.48059e-19
+
+ # var15, var16, var17, var18, var19, var20, var21, var22, var23, var24, var25, var26, var27, var28, var30, var31, var32, var33, var34, var35, var36, var37, var38, var39, var40
+  #var1, var2, var3, var4, var5, var6, var7, var8, var9, var10, var11, var12, var13, var14
   
   ####chain####
   
@@ -324,9 +449,24 @@ if (group == "All") {
   #BrapaP_for_MB$type_of_site=NULL
   
   ch1<-MCMCped(PdP=PdP, GdP=GdP, sP=sP.chain1, tP=tP,verbose=T,mm.tol=5, nitt=10000+(10000*10), thin=10*10, burnin=10000)
-quartz()
+  ch1.dur<-MCMCped(PdP=PdP, GdP=GdP, sP=sP.chain1, tP=tP,verbose=T,mm.tol=5, nitt=10000+(10000*10), thin=10*10, burnin=10000)
+  mod_subfolder<-"data_files_3/files_generated_by_PatAnalysis_Script"
+  save(ch1, file=paste(save_location, mod_subfolder, "m12.ch1.Rdata", sep="/"))
+  write.csv(ch1, file = "Fwest_sum_slices_ch1.csv")
+  save(ch1, file=paste(save_location,mod_subfolder, sep="/"))
+  save(ch1, file=paste(save_location, mod_subfolder, "ch1.Fwest.sum_slices.RData", sep="/"))
 summary(ch1$beta)
+quartz()
+summary(ch1.dur$beta)
 } else if (group == "Batch") {
+  #script for doing analysis just by batch 
+  BrapaG<-read.csv(paste("BrapaG_", Plot, ".csv", sep=""))
+  BrapaP<-read.csv(paste("BrapaP_", Plot, ".csv", sep=""))
+  BrapaG$X<-NULL
+  BrapaP$X.1<-NULL
+  BrapaG$sex<-NULL
+  BrapaG<-merge(BrapaP,BrapaG,by="id")
+  BrapaG<-subset(BrapaG, select=-c(offspring,terr,X,Y,ft,dur,tot_flwrs,sex))
 
 }
   
